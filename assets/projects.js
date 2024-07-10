363e87614eaaa166335c61f668e9a724487f1f6d
mlikayoussef <youssefmlika.lac2@gmail.com>
2024-07-10 10:33:48 +0200

    Updates

---
+++ assets/projects.js
@@ -0,0 +1,32 @@
+import projectImg1 from '../projects screenshots/e-commerce website.png'
+import  projectImg2 from '../projects screenshots/portfolio-img-project.png'
+import projectImg3 from '../projects screenshots/welcome.png'
+export const projects=[
+    {
+        number:1,
+        img:projectImg1,
+        title: 'E-commerce website',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas placeat aliquam pariatur sunt dolorum autem nobis illo neque incidunt, reprehenderit doloremque eaque libero rem deleniti vitae adipisci quod, illum quis!',
+        demoUrl:'https://668d2c3f6c69574f0a10a0b0--willowy-bonbon-000d6f.netlify.app/',
+        codeUrl:'https://github.com/CrypticCoder67458/E-commerce-app',
+        isWorking:true,
+    },
+    {
+        number:2,
+        img:projectImg2,
+        title: 'Portfolio',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas placeat aliquam pariatur sunt dolorum autem nobis illo neque incidunt, reprehenderit doloremque eaque libero rem deleniti vitae adipisci quod, illum quis!',
+        demoUrl:'/',
+        codeUrl:'https://github.com/CrypticCoder67458/react-projects/tree/main/portfolio',
+        isWorking:true,
+    },
+    {
+        number:3,
+        img:projectImg3,
+        title: 'Youtube Scrapper',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas placeat aliquam pariatur sunt dolorum autem nobis illo neque incidunt, reprehenderit doloremque eaque libero rem deleniti vitae adipisci quod, illum quis!',
+        demoUrl: 'https://bytepicks.com/',
+        codeUrl:'https://github.com/CrypticCoder67458/BytePicks-master/tree/master/BytePicks-master',
+        isWorking:false,
+    }
+]
